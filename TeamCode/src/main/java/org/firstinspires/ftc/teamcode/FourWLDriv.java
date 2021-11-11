87cc9a713cdf59c3340595113c52876568fec6e0
Robouser <mjsalasolvera@gmail.com>
2021-11-11 16:54:25 -0500

    Roman sux

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWLDriv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWLDriv.java
@@ -23,17 +23,20 @@ public class FourWLDriv extends HwMapIter{
     @Override
     public void loop()
     {
-        if(Math.abs(gamepad1.left_stick_y)>.1 || Math.abs(gamepad1.right_stick_y)>.1 ) {
+        if(Math.abs(gamepad1.left_stick_y)>.1) {
             fl.setPower(-gamepad1.left_stick_y);
-            fr.setPower(-gamepad1.right_stick_y);
-            //bl.setPower(-gamepad1.left_stick_y);
-            //br.setPower(-gamepad1.right_stick_y);
+            bl.setPower(-gamepad1.right_stick_y);
         }
-
         else {
             fl.setPower(0);
-            fr.setPower(0);
             bl.setPower(0);
+        }
+        if(Math.abs(gamepad1.right_stick_y)>.1) {
+            fr.setPower(-gamepad1.right_stick_y);
+            br.setPower(-gamepad1.right_stick_y);
+        }
+        else {
+            fr.setPower(0);
             br.setPower(0);
         }
 
